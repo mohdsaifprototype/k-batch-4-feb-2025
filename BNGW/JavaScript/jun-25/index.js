f9329ce3b1e66819194a16b147df11d97241a490
Mohd Saif <mohdsaifsiddique.feaindia@gmail.com>
2025-06-27 17:28:26 +0530

    June 25
    JS Operators

---
+++ BNGW/JavaScript/jun-25/index.js
@@ -0,0 +1,61 @@
+/* let apples = "2";
+let oranges = "3";
+
+// both values converted to numbers before the binary plus
+console.log( +apples + +oranges ); // 5
+
+// the longer variant
+// console.log( Number(apples) + Number(oranges) ); // 5 */
+
+/* Program to swap the values of two variables */
+// let a = 5;
+// let b = 6;
+// console.log("Value of a:", a);
+// console.log("Value of b:", b);
+
+// // let temp;
+// // temp = a;
+// // a = b;
+// // b = temp;
+
+// /* a = a + b;
+// b = a - b;
+// a = a - b; */
+
+// console.log("Values after swap");
+// console.log("Value of a:", a);
+// console.log("Value of b:", b);
+
+// let age = 22;
+// console.log(`You are ${age} and next year you shall be ${age++}`);
+// console.log(`You are ${age} and next year you shall be ${++age}`);
+
+// let age = 22;
+// console.log(age++);  // 22
+// console.log(age--);  // 23
+// console.log(++age);  // 23
+// console.log(age--);  // 23
+// console.log(age++);  // 22
+// console.log(age++);  // 23
+// console.log(--age);  // 23
+// console.log(age++);  // 23
+// console.log(++age);  // 25
+// console.log(age++);  // 25
+// console.log(age);    // 26
+
+// console.log("" + 1 + 0);     // '10'
+// console.log("" - 1 + 0);     // -1
+// console.log(true + false);   // 1
+// console.log(6 / "3");        // 2
+// console.log("2" * "3");      // 6
+// console.log(4 + 5 + "px");   // '9px'
+// console.log("$" + 4 + 5);    // '$45'
+// console.log("4" - 2);        // 2
+// console.log("4px" - 2);      // NaN
+// console.log("  -9  " + 5);   // '  -9  5'
+// console.log("  -9  " - 5);   // -14
+// console.log(null + 1);       // 1
+// console.log(undefined + 1);  // NaN
+// console.log(" \t \n" - 2);   // -2
+
+console.log("You must have a \u0022credit card\u0022, if you want to shop on the \"Internet\".");
