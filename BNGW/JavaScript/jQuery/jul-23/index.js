ff7312c9ea86f26f0df71e5acf8f38f1a2dde6ed
Mohd Saif <mohdsaifsiddique.feaindia@gmail.com>
2025-07-28 18:32:21 +0530

    July 28
    JQuery Quiz and JS if else, if else if else ladder

--- BNGW/JavaScript/jQuery/jul-23/index.js
+++ BNGW/JavaScript/jQuery/jul-23/index.js
@@ -1,9 +1,39 @@
 $("document").ready(function () {
-  $("button[data-check]").click(function () {
-    console.log(
-      $(this)
-        .prevUntil("h6")
-        .children("input:checked").val()
-    );
+  let i = 0;
+  $("[data-append]").click(function () {
+    // Gathering Data from HTML and storing in the following variable
+    let q = $("#q").val();
+    let op1 = $("#op1").val();
+    let op2 = $("#op2").val();
+    let op3 = $("#op3").val();
+    let op4 = $("#op4").val();
+    // Putting those data into the below string template
+    let myQuestDOM = `
+  <div id="q${i++}" class="question">
+    <!-- Question -->
+    <h6>${q}</h6>
+    <!-- options -->
+    <label><input type="radio" name="q1" value="${op1}"> ${op1}</label>
+    <label><input type="radio" name="q1" value="${op2}"> ${op2}</label>
+    <label><input type="radio" name="q1" value="${op3}"> ${op3}</label>
+    <label><input type="radio" name="q1" value="${op4}"> ${op4}</label>
+    <!-- Buttons -->
+    <button data-check class="btn btn-secondary" type="button">Check Answer</button>
+  </div>
+  `;
+  // Finally appending the question using the above string template
+    $(".quiz").append(myQuestDOM);
   });
+
+  // $("button[data-check]").click(function () {
+  //   console.log(
+  //     $(this)
+  //       .prevUntil("h6")
+  //       .children("input:checked").val()
+  //   );
+  // });
 });
+
+/* 
+To underestand the above code refer to String Template aka Template litral.
+*/
---
+++ BNGW/JavaScript/jul-28/index.js
@@ -0,0 +1,66 @@
+/* 
+Topic: conditional statement
+
+Syntax:
+if (codition) {
+  Statements...
+}
+*/
+// Program to check if a person is eligible to vote?
+
+// let usrAge = 18;
+
+// if (usrAge >= 18) {
+//   console.log("You are eligible to vote!");
+// }
+// else {
+//   console.log("You are ineligible to vote");
+// }
+
+// Write a program to check if a no. is even?
+// let num = 0;
+// if (num % 2 == 0) {
+//   console.log("number is even");
+// } else {
+//   console.log("number is odd");
+// }
+
+/* 
+Topic: if, else if, else ladder.
+*/
+// Write a program to check if a no. is even, odd, zero.
+// let num = 12;
+// if (num == 0) {
+//   console.log("No. is zero");
+// } else if (num % 2 == 0) {
+//   console.log("Even number!");
+// } else {
+//   console.log("Odd number!");
+// }
+
+/*
+  Write a program to do the following
+  1. If the no. is even print it's half value
+  2. If the no. is odd print it's double value
+  3. If it's zero print '0'
+  4. If the value is -ve print it's absolute value
+*/
+let num = 0;
+if(num == 0){
+  console.log("The number is zero: ZERO ");
+}else if(num<0){
+  console.log("This number is negative and u trick me", num*-1);
+}else if(num%2 == 0){
+  console.log("The number is  even", num/2 );
+}
+else{
+  console.log("The number is odd:", num*2);
+}
+
+/* 
+  Write a program to do the check if a no. is
+  1. even print it's half value
+  2. odd print it's double value
+  3. zero print '0'
+  4. -ve print it's absolute value
+*/
