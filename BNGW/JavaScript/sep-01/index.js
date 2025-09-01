ec10ab51a72f96e80c15d59f91adf30b828592a9
Mohd Saif <mohdsaifsiddique.feaindia@gmail.com>
2025-09-01 18:19:40 +0530

    September 01
    String and it's methods

---
+++ BNGW/JavaScript/sep-01/index.js
@@ -0,0 +1,30 @@
+/* 
+Topic: String and it's method.
+String is a collection of more than 1 character stored as a single value.
+
+We represent string using quotes
+i.e.,
+'This is a string'
+"This is a string"
+
+There is a a special type of string aka string template or template litral.
+We use baktic for that.
+e.g.,
+let usr = "Ankush";
+let message = `Hi there ${usr}, learning JavaScript is fun.`;
+console.log(message);
+*/
+
+// let str = "Hello there!";
+// console.log(str.length);
+
+// console.log(str[str.length - 1]);
+// console.log(str.slice(6,11));
+// console.log("Uppercase:", str.toUpperCase());
+// console.log("Lowercase:", str.toLowerCase());
+
+let str = 'Widget with id';
+
+console.log( str.indexOf('Widget') ); // 0, because 'Widget' is found at the beginning
+console.log( str.indexOf('wwidget') ); // -1, not found, the search is case-sensitive
+console.log( str.indexOf("with") ); // 1, "id" is found at the position 1 (..idget with id)
