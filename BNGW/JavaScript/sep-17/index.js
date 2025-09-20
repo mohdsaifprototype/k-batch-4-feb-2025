f8dfdf9b4a49a8eb5ead3e53d4af4f8a9173c697
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2025-09-20 16:10:15 +0530

    September 17
    JS Analogue clock hint

---
+++ BNGW/JavaScript/sep-17/index.js
@@ -0,0 +1,8 @@
+console.log("Fetching data...");
+let var1 = setTimeout(() => {
+  console.log("Data printed successfully!");
+}, 2000);
+
+console.log("data fetched 1...");
+
+// clearTimeout(var1);
